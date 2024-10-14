31d245238eeffaa2b6bb63dc83aea93d068fde82
Ivan Polovynka <ivanpolovynka@gmail.com>
2024-10-14 13:01:41 +0300

    update database

--- src/app/db/index.ts
+++ src/app/db/index.ts
@@ -1,9 +1,7 @@
-import Database from 'better-sqlite3';
+import { sql } from '@vercel/postgres';
+
+import { drizzle } from 'drizzle-orm/vercel-postgres';
 
 import { config } from 'dotenv';
-
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-config({ path: '.env.local' });
-
-const sqlite = new Database('local.db');
-export const db = drizzle(sqlite);
+config({ path: '.env.local' }); // or .env
+export const db = drizzle(sql);
